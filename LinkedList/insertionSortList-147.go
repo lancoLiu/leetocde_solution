472e196713d02540305776a687afbbfbfdebc318
lanco <lancowhatyouneed@gmail.com>
2020-10-24 21:50:05 +0800

    add 147 comment

--- LinkedList/insertionSortList-147.go
+++ LinkedList/insertionSortList-147.go
@@ -10,26 +10,31 @@ package LinkedList
 //插入排序
 
 func insertionSortList(head *ListNode) *ListNode {
-	/*	dummy := &ListNode{}
-		curr := dummy
-		curr.Next = head*/
 	if head == nil || head.Next == nil {
 		return head
 	}
 	dummy := &ListNode{}
 	dummy.Next = head
+	//tail指向排好序的尾节点，sort是下一个待排序的节点
 	tail := head
 	sort := head.Next
 
 	for sort != nil {
 		if sort.Val < tail.Val {
+			//每次重新重dummy出发找到本节点该插入的位置
 			pos := dummy
 			for pos.Next.Val < sort.Val {
 				pos = pos.Next
 			}
+			//dummy-》4-》1-》3-》2为例子
+
+			//排好序的尾节点指向待排序节点的下一个节点（4指向1，2为本轮待排序节点）
 			tail.Next = sort.Next
+			//pos此时在dummy处，先让这轮排序的节点连接它的下一个节点，以免丢失。即2连接4
 			sort.Next = pos.Next
+			//dummy-》2
 			pos.Next = sort
+			//下一个待排序节点
 			sort = tail.Next
 		} else {
 			tail = tail.Next
