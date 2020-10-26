2f58950fc0c57ed414a3db8a45396e3ce39499d3
lanco <lancowhatyouneed@gmail.com>
2020-10-26 21:00:45 +0800

    solve 226

---
+++ Tree/invertTree-226.go
@@ -0,0 +1,13 @@
+package Tree
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+
+}
