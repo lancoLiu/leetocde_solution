569e849a2c44c87e1fdea4a131464209428804c2
lanco <lancowhatyouneed@gmail.com>
2020-11-08 18:30:57 +0800

    solve 617

---
+++ hot100/mergeTrees-617.go
@@ -0,0 +1,41 @@
+package hot100
+
+//https://leetcode-cn.com/problems/merge-two-binary-trees/solution/dong-hua-yan-shi-di-gui-die-dai-617he-bing-er-cha-/
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+
+你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。
+
+就像合并两个链表一样
+*/
+func mergeTrees(t1 *TreeNode, t2 *TreeNode) *TreeNode {
+	if t1 == nil {
+		return t2
+	}
+	if t2 == nil {
+		return t1
+	}
+	return dfs(t1, t2)
+
+}
+
+func dfs(t1 *TreeNode, t2 *TreeNode) *TreeNode {
+	if t1 == nil {
+		return t2
+	}
+	if t2 == nil {
+		return t1
+	}
+	t1.Val += t2.Val
+
+	//递归
+	t1.Left = dfs(t1.Left, t2.Left)
+	t1.Right = dfs(t1.Right, t2.Right)
+	return t1
+}
