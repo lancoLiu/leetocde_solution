2b9aefd326f621af2b438043e374d8194b6be969
liujinfan <liujinfan@baidu.com>
2021-06-13 14:58:26 +0800

    add 4

--- binarySearch/4/leetcode-4.go
+++ binarySearch/4/leetcode-4.go
@@ -80,6 +80,25 @@ func findMedianSortedArrays2(nums1 []int, nums2 []int) float64 {
 		return float64(getKthElement(nums1, nums2, midIndex1+1)+getKthElement(nums1, nums2, midIndex2+1)) / 2.0
 	}
 }
+
+/**
+根据二分查找第k个小的数
+查找数组A的第k / 2小与B数组的第k / 2小
+1、如果A[k/2 - 1] < B[k /2 -1]
+说明A数组的前k/2-1数都不是答案，可以排除掉
+排除之后两个数组的大小变了，k值更新，因为排除掉A数组的k/2个元素。所以新的k应该为k-k/2
+
+2、如果B[k/2 - 1] < A[k / 2 - 1],如上分析
+
+3、A[k/2 - 1] == B[k /2 -1]，归入第一种情况
+
+考虑上面三种情况的特殊情况
+1、A[k/2-1]或者B[k/2-1]越界了，那就选择对应数组的最后一个元素，再根据排除数做减法
+//例如oldIndex1 + k/2是越界的，那我取A数组的最后元素index1 = len(A)-1,
+如果A[index1] <= B[index2]（index2没有越界）,那相应的k应该更新为k = k - (index1-oldIndex1 + 1)
+
+2、第二种如果一个数组为空了，即index1 = len(A)，就可以返回另一个数组的第k小元素
+*/
 func getKthElement(nums1, nums2 []int, k int) int {
 	index1, index2 := 0, 0
 	for {
@@ -99,6 +118,7 @@ func getKthElement(nums1, nums2 []int, k int) int {
 		if pivot1 <= pivot2 {
 			//考虑A[k / 2 - 1]越界了，根据排除数减少个数，而不能直接将K减去k/2
 			k -= (newIndex1 - index1 + 1)
+			//往右移动，可能超出数组大小
 			index1 = newIndex1 + 1
 		} else {
 			k -= (newIndex2 - index2 + 1)
