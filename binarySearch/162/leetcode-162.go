d82f1cecca792adc365b1addd39cfb4a2981f506
lanco <lancowhatyouneed@gmail.com>
2021-07-06 22:27:14 +0800

    162

---
+++ binarySearch/162/leetcode-162.go
@@ -0,0 +1,23 @@
+package main
+
+//峰值元素是指其值大于左右相邻值的元素。
+
+//给你一个输入数组 nums，找到峰值元素并返回其索引。数组可能包含多个峰值，在这种情况下，返回 任何一个峰值 所在位置即可。
+
+//你可以假设 nums[-1] = nums[n] = -∞ 。
+
+func findPeakElement(nums []int) int {
+	left, right := 0, len(nums)-1
+	for left < right {
+		mid := left + (right-left)/2
+		if nums[mid] < nums[mid+1] {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	return left
+}
+func main() {
+
+}
