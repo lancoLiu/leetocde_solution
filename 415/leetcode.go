83c8f55e4e938baf70d924526b49dc35aea2e115
Lancoliu <liujinfan@baidu.com>
2021-07-18 11:20:12 +0800

    415

---
+++ 415/leetcode.go
@@ -0,0 +1,33 @@
+package main
+
+import "strconv"
+
+//给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和。
+
+/**
+num1 和num2 的长度都小于 5100
+num1 和num2 都只包含数字 0-9
+num1 和num2 都不包含任何前导零
+*/
+func addStrings(num1 string, num2 string) string {
+	//字符串形式的非负整数，没有负号，没有前导0
+	//从后往前加，使用双指针形式记录位置
+	lens1 := len(num1) - 1
+	lens2 := len(num2) - 1
+	ans := ""
+	add := 0
+	for i, j := lens1, lens2; i >= 0 || j >= 0 || add != 0; i, j = i-1, j-1 {
+		var x, y int
+		if i >= 0 {
+			x = int(num1[i] - '0')
+		}
+		if j >= 0 {
+			y = int(num2[j] - '0')
+		}
+		result := x + y + add
+		ans = strconv.Itoa(result%10) + ans
+		add = result / 10
+	}
+
+	return ans
+}
