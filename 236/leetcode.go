b623a70d46101ea1f6dc73906e4b01ad5932ae16
Lancoliu <liujinfan@baidu.com>
2021-07-20 10:31:30 +0800

    m 236

--- 236/leetcode.go
+++ 236/leetcode.go
@@ -20,11 +20,14 @@ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
 	left := lowestCommonAncestor(root.Left, p, q)
 	right := lowestCommonAncestor(root.Right, p, q)
 	//这里比较难想
+	//当 left为空 ，right不为空
 	//当left为空，说明p,q都不在左子树中
 	//讨论：
 	//1、p,q 其中一个在root的右子树中，此时 right指向p(假设为p)；
 	//2、p,q 两节点都在 root的右子树中，此时的right指向最近公共祖先节点 ；
-	//3、两个都不在root的左右子树中，向上返回
+	if left == nil && right == nil {
+		return nil // 1.
+	}
 	if left == nil {
 		return right
 	}
