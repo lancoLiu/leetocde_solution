4c4f1bb96f964eb5b5d079dc3a03a7e5c752e6cc
Lancoliu <liujinfan@baidu.com>
2021-07-23 19:47:47 +0800

    94

---
+++ 94/leetcode.go
@@ -0,0 +1,39 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+//中序遍历
+func inorderTraversal(root *TreeNode) (res []int) {
+
+	var inorder func(node *TreeNode)
+	inorder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		inorder(node.Left)
+		res = append(res, node.Val)
+		inorder(node.Right)
+	}
+	inorder(root)
+	return
+}
+
+//迭代
+func inorderTraversal2(root *TreeNode) (res []int) {
+	stack := []*TreeNode{}
+	for root != nil || len(stack) > 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, root.Val)
+		root = root.Right
+	}
+	return
+}
