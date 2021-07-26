e4eca91820f41cd5fa2dfd7f4efa6f8dc8b477f1
Lancoliu <liujinfan@baidu.com>
2021-07-26 19:53:06 +0800

    2

---
+++ 2/leetcode.go
@@ -0,0 +1,59 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	carry := 0
+	dummy := &ListNode{}
+	res := dummy
+
+	for l1 != nil || l2 != nil || carry !=  0 {
+		if l1 != nil {
+			carry += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			carry += l2.Val
+			l2 = l2.Next
+		}
+
+		tmp := &ListNode{
+			Val: carry % 10,
+			Next: nil,
+		}
+		res.Next = tmp
+		res = res.Next
+		carry /=  10
+
+	}
+	return dummy.Next
+}
+返回该题
+
+func main() {
+	l1 := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val:  3,
+				Next: nil,
+			},
+		},
+	}
+
+	l2 := &ListNode{
+		Val: 4,
+		Next: &ListNode{
+			Val: 5,
+			Next: &ListNode{
+				Val:  6,
+				Next: nil,
+			},
+		},
+	}
+	addTwoNumbers(l1, l2)
+}
